216748735b383e57346735d8c5c866cdf2bd6337
korolevakrasoti <64890753+korolevakrasoti@users.noreply.github.com>
2020-10-21 21:37:35 +0300

    Add files via upload

---
+++ createBD.sql
@@ -0,0 +1,10 @@
+
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
